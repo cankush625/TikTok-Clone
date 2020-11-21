97964bc1259907d2faf7e22f9ebeae7945dc341e
cankush625 <cankush625@gmail.com>
2020-11-22 01:46:22 +0530

    TiTok Video Player is created
    Signed-off-by: cankush625 <cankush625@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
+import Video from "./Video";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
+    <div className="app">
+      <div classname="app__videos">
+        <Video />
+        <Video />
+      </div>
     </div>
   );
 }
---
+++ src/Video.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./Video.css";
+
+function Video() {
+    return (
+        <div className="video">
+            <video
+                className="video__player"
+                loop
+                src=""
+            >
+
+            </video>
+
+            {/* VideoFooter */}
+            {/* VideoSidebar */}
+        </div>
+    )
+}
+
+export default Video

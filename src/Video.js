513cb78c6f2ef6b36510e72a513fa6667c216152
cankush625 <cankush625@gmail.com>
2020-12-01 23:42:55 +0530

    Video player created
    Signed-off-by: cankush625 <cankush625@gmail.com>

--- src/Video.js
+++ src/Video.js
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 import "./Video.css";
 
 function Video() {
+    const [playing, setPlaying] = useState(false);
+    const videoRef = useRef(null);
+
+    const handleVideoPress = () => {
+        if (playing) {
+            videoRef.current.pause();
+            setPlaying(false);
+        } else {
+            videoRef.current.play();
+            setPlaying(true);
+        }
+    };
+
     return (
         <div className="video">
             <video
+                onClick={handleVideoPress}
                 className="video__player"
                 loop
+                ref={videoRef}
                 src=""
             >
 
---
+++ src/VideoSidebar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./VideoSidebar.css";
+
+function VideoSidebar() {
+    return (
+        <div class="videoSidebar">
+            
+        </div>
+    )
+}
+
+export default VideoSidebar

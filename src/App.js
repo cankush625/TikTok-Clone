9c2acfefbe7a4ceb43b36540c24c5f525aa420cd
cankush625 <cankush625@gmail.com>
2020-12-08 01:52:01 +0530

    Axios file added and frontend and backend is connected
    Signed-off-by: cankush625 <cankush625@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Video from "./Video";
+import axios from "./axios";
 
 function App() {
+
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const response = await axios.get("/v2/posts");
+      setVideos(response.data);
+
+      return response;
+    }
+
+    fetchPosts();
+  }, []);
+
   return (
     <div className="app">
       <div classname="app__videos">
-        <Video
-          url=""
-          channel='demoacc'
-          description="This is the description"
-          song="This is the song"
-          likes={4132}
-          messages={243}
-          shares={325}
-        />
-        <Video
-          url=""
-          channel='acc'
-          description="This is the description"
-          song="This is the song"
-          likes={253}
-          messages={41}
-          shares={143}
-        />
+        {videos.map(({url, channel, description, song, likes, messages, shares}) => (
+          <Video
+            url = {url}
+            channel = {channel}
+            description = {description}
+            song = {song}
+            likes = {likes}
+            messages = {messages}
+            shares = {shares}
+          />
+        ))};
       </div>
     </div>
   );
